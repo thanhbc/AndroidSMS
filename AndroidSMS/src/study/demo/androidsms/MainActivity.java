f1ac689d4efb90c913272efbb51c8177338c2ab9
thanhbc <thanhbc1990@gmail.com>
2013-08-20 15:45:32 +0700

    Add SMSReceiver Class
    Signed-off-by: thanhbc <thanhbc1990@gmail.com>

--- AndroidSMS/src/study/demo/androidsms/MainActivity.java
+++ AndroidSMS/src/study/demo/androidsms/MainActivity.java
@@ -1,15 +1,110 @@
 package study.demo.androidsms;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+	public static String SENT = "SMS_SENT";
+	public static String DELIVERED = "SMS_DELIVERED";
+	Button btnSend;
+	EditText edtMessage;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		edtMessage = (EditText) findViewById(R.id.edtMessage);
+		btnSend = (Button) findViewById(R.id.btnSend);
+		btnSend.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				 sendSMS("5554", edtMessage.getText().toString());
+//				Intent intent = new Intent(android.content.Intent.ACTION_VIEW);
+//				intent.putExtra("address", "1234; 5678; 0987");
+//				intent.putExtra("sms_body", edtMessage.getText().toString());
+//
+//				intent.setType("vnd.android-dir/mms-sms");
+//				startActivity(intent);
+
+			}
+		});
+	}
+
+	private void sendSMS(String phoneNumber, String message) {
+		PendingIntent sendPI = PendingIntent.getBroadcast(this, 0, new Intent(
+				SENT), 0);
+
+		PendingIntent deliveredPI = PendingIntent.getBroadcast(this, 0,
+				new Intent(DELIVERED), 0);
+
+		// When the SMS has been sent
+		registerReceiver(new BroadcastReceiver() {
+
+			@Override
+			public void onReceive(Context context, Intent intent) {
+				switch (getResultCode()) {
+				case Activity.RESULT_OK:
+					Toast.makeText(getBaseContext(), "SMS sent",
+							Toast.LENGTH_LONG).show();
+					break;
+				case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+					Toast.makeText(getBaseContext(), "Generic failure",
+							Toast.LENGTH_LONG).show();
+					break;
+				case SmsManager.RESULT_ERROR_NO_SERVICE:
+					Toast.makeText(getBaseContext(), "No Service",
+							Toast.LENGTH_LONG).show();
+					break;
+				case SmsManager.RESULT_ERROR_NULL_PDU:
+					Toast.makeText(getBaseContext(), "Null PDU",
+							Toast.LENGTH_LONG).show();
+					break;
+				case SmsManager.RESULT_ERROR_RADIO_OFF:
+					Toast.makeText(getBaseContext(), "Radio off",
+							Toast.LENGTH_LONG).show();
+					break;
+				}
+
+			}
+
+		}, new IntentFilter(SENT));
+
+		// When the SMS has been delivered
+		registerReceiver(new BroadcastReceiver() {
+
+			@Override
+			public void onReceive(Context context, Intent intent) {
+				switch (getResultCode()) {
+				case Activity.RESULT_OK:
+					Toast.makeText(getBaseContext(), "SMS delivered",
+							Toast.LENGTH_LONG).show();
+					break;
+				case Activity.RESULT_CANCELED:
+					Toast.makeText(getBaseContext(), "SMS not delivered",
+							Toast.LENGTH_LONG).show();
+
+				}
+
+			}
+
+		}, new IntentFilter(DELIVERED));
+
+		SmsManager sms = SmsManager.getDefault();
+		sms.sendTextMessage(phoneNumber, null, message, sendPI, deliveredPI);
 	}
 
 	@Override
